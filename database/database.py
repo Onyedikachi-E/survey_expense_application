3ff2280b6eaa08b9574dd7bd0120c8df40935d91
Onyedikachi-E <onyedika.ezeobi@gmail.com>
2024-10-21 23:14:04 +0100

    Final Project

--- database/database.py
+++ database/database.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
+# Connection string to MongoDB Atlas
 uri = "mongodb+srv://admin:admin123@servermanager.j0y1z.mongodb.net/survey_manager?retryWrites=true&w=majority&appName=servermanager"
 
 # Set up an instance of the MongoClient()
 client = MongoClient(uri, server_api=ServerApi('1'))
-# client = MongoClient(host="localhost", port=27017)
 
 # Initialize the database
 db = client.survey_manager
