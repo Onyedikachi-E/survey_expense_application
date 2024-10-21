7aba1503acd44b99dcaabe4690a3555b30d4a5c1
Onyedikachi-E <onyedika.ezeobi@gmail.com>
2024-10-21 20:14:22 +0100

    Final Project

--- database/database.py
+++ database/database.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://admin:admin123@servermanager.j0y1z.mongodb.net/?retryWrites=true&w=majority&appName=servermanager"
+uri = "mongodb+srv://admin:admin123@servermanager.j0y1z.mongodb.net/survey_manager?retryWrites=true&w=majority&appName=servermanager"
 
 # Set up an instance of the MongoClient()
 client = MongoClient(uri, server_api=ServerApi('1'))
